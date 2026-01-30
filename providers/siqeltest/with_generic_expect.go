b65cce3e86307d3b2fbe8797dbe9fc3425b1ef5c
João Victor Oliveira Couto <commits@jictyvoo.dev>
2026-01-30 19:36:57 -0300

    :sunrise: feat: Add a new provider to allow using generic db-validator

---
+++ providers/siqeltest/with_generic_expect.go
@@ -0,0 +1,46 @@
+package siqeltest
+
+import (
+	"database/sql"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/vinovest/sqlx"
+
+	"github.com/wrapped-owls/testereiro/puppetest/pkg/runners/dbrunner"
+	"github.com/wrapped-owls/testereiro/puppetest/pkg/runners/dbrunner/sanitizers"
+)
+
+// WithExpect adds a validator that queries the database and compares the result with the expected object.
+func WithExpect[O any](expected O, sanitizer sanitizers.DbSanitizer[O]) dbrunner.Option {
+	return func(modifier dbrunner.RunnerModifier) {
+		modifier.AddValidator(&expectValidator[O]{
+			expected:  expected,
+			sanitizer: sanitizer,
+		})
+	}
+}
+
+type expectValidator[O any] struct {
+	expected  O
+	sanitizer sanitizers.DbSanitizer[O]
+}
+
+func (v *expectValidator[O]) SelectionFields() string {
+	return "*"
+}
+
+func (v *expectValidator[O]) Validate(t testing.TB, rows *sql.Rows) error {
+	if !rows.Next() {
+		return fmt.Errorf("no records found")
+	}
+
+	var destination O
+	if err := sqlx.StructScan(rows, &destination); err != nil {
+		return err
+	}
+
+	assert.Equal(t, v.expected, destination)
+	return nil
+}
